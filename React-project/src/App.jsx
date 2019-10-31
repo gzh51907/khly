dbbefcaf2f02e4506f618695dff583aa0db7ab9b
superwang <2361894280@qq.com>
2019-10-31 20:34:05 +0800

    20.33

--- React-project/src/App.jsx
+++ React-project/src/App.jsx
@@ -8,6 +8,7 @@ import Reg from '@@/Reg';
 import Login from '@@/Login';
 import Goods from '@@/Goods';
 import List from '@@/List';
+import Order from '@@/Order';
 import './App.scss';
 
 @withRouter
@@ -21,9 +22,11 @@ class App extends Component {
                     <Route path="/login" component={Login} />
                     <Route path="/list" component={List} />
                     <Route path="/goods/:id" component={Goods} />
-                    <Route path="/NotFound" render={() => <div><h1 style={{ textAlign: 'center' }}>404</h1></div>} />
+                    <Route path="/order/:id" component={Order} />
+                    <Route path="/NotFound" render={() => <div><h1 style={{ textAlign: 'center' }}>404</h1></div>} /> */}
                     <Redirect from="/" to="/home" exact />
                     <Redirect from="*" to="/NotFound" />
+
                 </Switch>
             </>
         )
